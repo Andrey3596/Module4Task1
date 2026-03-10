2cd43b99c02046ca1b50796693e3d7a447358bd4
Limolix <andrejnagirnak395@gmail.com>
2026-03-10 20:46:28 +0800

    add parent class

--- task1/Film.cs
+++ task1/Film.cs
@@ -7,20 +7,26 @@ using System.Threading.Tasks;
 
 namespace task1
 {
-    public class Movie
+
+
+    public class Film
+    {
+    }
+
+    public class Movie : Film
     {
         public double Timekeeping = 0;
         public int NumberAwards = 0;
         
     }
 
-    public class Series
+    public class Series : Film
     {
         public int NumberSeasons = 0;
         public int BonesNumber = 0;
     }
 
-    public class Telecast
+    public class Telecast : Film
     {
         public double Duration = 0;
         public double AirTime = 0;

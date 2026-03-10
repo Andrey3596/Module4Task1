2b270e27fb250c5dc74acc9639071ca215990f25
Limolix <andrejnagirnak395@gmail.com>
2026-03-10 21:30:41 +0800

    del comments

--- task1/Form1.cs
+++ task1/Form1.cs
@@ -17,19 +17,19 @@ namespace task1
 
             for (var i = 0; i < 10; ++i)
             {
-                switch (rnd.Next() % 3) // генерирую случайное число от 0 до 2 (ну остаток от деления на 3)
+                switch (rnd.Next() % 3)
                 {
-                    case 0: // если 0, то мандарин
+                    case 0: 
                         this.filmList.Add(Movie.Generate());
                         break;
-                    case 1: // если 1 то виноград
+                    case 1: 
                       
                         this.filmList.Add(Series.Generate());
                         break;
-                    case 2: // если 2 то арбуз
+                    case 2: 
                         this.filmList.Add(Telecast.Generate());
                         break;
-                        // появление других чисел маловероятно
+                        
                 }
             }
             ShowInfo();
@@ -37,19 +37,17 @@ namespace task1
 
         private void ShowInfo()
         {
-            // заведем счетчики под каждый тип
+            
             int movieCount = 0;
             int seriesCount = 0;
             int telecastCount = 0;
 
-            // пройдемся по всему списку
+            
             foreach (var film in this.filmList)
             {
-                // помните, что в списки у нас лежат фрукты,
-                // то есть объекты типа Fruit
-                // поэтому чтобы проверить какой именно фрукт
-                // мы в данный момент обозреваем, мы используем ключевое слово is
-                if (film is Movie) // читается почти как чистый инглиш, "если fruit есть Мандарин"
+                
+                
+                if (film is Movie) 
                 {
                     movieCount += 1;
                 }
@@ -63,8 +61,8 @@ namespace task1
                 }
             }
 
-            // а ну и вывести все это надо на форму
-            txtInfo.Text = "Фильм\tСриал\tПрдач"; // буквы экнмлю, чтобы влезло на форму
+            
+            txtInfo.Text = "Фильм\tСриал\tПрдач";
             txtInfo.Text += "\n";
             txtInfo.Text += String.Format("{0}\t{1}\t{2}", movieCount, seriesCount, telecastCount);
         }
@@ -77,15 +75,13 @@ namespace task1
                 return;
             }
 
-            // взяли первый фрукт
+            
             var film = this.filmList[0];
-            // тут вам не реальность, взятие это на самом деле создание указателя на область в памяти
-            // где хранится экземпляр класса, так что если хочешь удалить, делай это сам
+            
             this.filmList.RemoveAt(0);
 
             txtOut.Text = film.GetInfo();
 
-            // обновим информацию о количестве товара на форме
             ShowInfo();
         }
     }

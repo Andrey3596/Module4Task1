58ad713b9d66cbea343730ff3566d70609314f95
Limolix <andrejnagirnak395@gmail.com>
2026-03-10 20:40:53 +0800

    add classes

--- task1/Film.cs
+++ task1/Film.cs
@@ -4,8 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// БУДТЕ ВНИМАТЕЛЬНЕЕ: ТУТ ДОЛЖЕН СТОЯТЬ ТОТ ЖЕ namespace что и в Program.cs
+
 namespace task1
 {
+    public class Movie
+    {
+        public double Timekeeping = 0;
+        public int NumberAwards = 0;
+        
+    }
 
+    public class Series
+    {
+        public int NumberSeasons = 0;
+        public int BonesNumber = 0;
+    }
+
+    public class Telecast
+    {
+        public double Duration = 0;
+        public double AirTime = 0;
+    }
 }

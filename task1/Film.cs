36dd3d4806a5222fcf03bc5c27e9386c1ef0c383
Limolix <andrejnagirnak395@gmail.com>
2026-03-10 20:34:29 +0800

    add file for class

---
+++ task1/Film.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// БУДТЕ ВНИМАТЕЛЬНЕЕ: ТУТ ДОЛЖЕН СТОЯТЬ ТОТ ЖЕ namespace что и в Program.cs
+namespace task1
+{
+
+}

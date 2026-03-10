b2159025c2aa6556c4a632b6453cf7e60ec02aa1
Limolix <andrejnagirnak395@gmail.com>
2026-03-10 20:56:21 +0800

    add ShowInfo

--- task1/Form1.cs
+++ task1/Form1.cs
@@ -11,7 +11,48 @@ namespace task1
 
         private void btnRefill_Click(object sender, EventArgs e)
         {
-
+            this.filmList.Clear();
+            for (var i = 0; i < 10; ++i)
+            {
+                // классно да, список типа Fruit, а кладем Mandarin
+                // вот она: "сила наследования"
+                this.filmList.Add(new Movie());
+            }
         }
+
+        private void ShowInfo()
+        {
+            // заведем счетчики под каждый тип
+            int movieCount = 0;
+            int seriesCount = 0;
+            int telecastCount = 0;
+
+            // пройдемся по всему списку
+            foreach (var film in this.filmList)
+            {
+                // помните, что в списки у нас лежат фрукты,
+                // то есть объекты типа Fruit
+                // поэтому чтобы проверить какой именно фрукт
+                // мы в данный момент обозреваем, мы используем ключевое слово is
+                if (film is Movie) // читается почти как чистый инглиш, "если fruit есть Мандарин"
+                {
+                    movieCount += 1;
+                }
+                else if (film is Series)
+                {
+                    seriesCount += 1;
+                }
+                else if (film is Telecast)
+                {
+                    telecastCount += 1;
+                }
+            }
+
+            // а ну и вывести все это надо на форму
+            txtInfo.Text = "фильмы\tсериалы\tтв"; // буквы экнмлю, чтобы влезло на форму
+            txtInfo.Text += "\n";
+            txtInfo.Text += String.Format("{0}\t{1}\t{2}", mandarinsCount, grapesCount, watermellonsCount);
+        }
+
     }
 }
